35a2d1ab5841323447dc8cf9554b712b2fac3b7b
Leo Irakliotis <lgreco@gmail.com>
2025-06-02 18:31:25 +0000

    voters

--- JUN02.py
+++ JUN02.py
@@ -19,13 +19,17 @@ def form_letter(recipient, promise):
     print()
 
 # Simplistic dictionary to demonstrate the concept. Using first names as keys in 
-# a dictionary is not a good idea. Keys must be unique and names are not.
+# a dictionary is not a good idea. Keys must be unique and names are not. There 
+# are two voters with the same name below, but only one of them will receive a
+# form letter -- the most recent one with the same name.
 
 my_voters = {
+    # KEY     : VALUE (or values)
     "Richard" : "build a park in your backyard",
     "Lula"    : "fix your sidewalk",
     "Ana"     : "call a friend in DC for a pardon",
-    "Cesar"   : "add a bus stop in your living room"
+    "Cesar"   : "add a bus stop in your living room",
+    "Ana"     : "fix the potholes" 
 }
 
 # for every voter:
@@ -34,3 +38,42 @@ my_voters = {
 
 for name, promise in my_voters.items():
     form_letter(name, promise)
+
+# A better dictionary using objects -- this is advanced stuff and shown here
+# as a reference and a goal for the course.
+
+# Create an object for voters. Each voter is defined by personal information (name,
+# address, and the promise made to them).
+class Voter:
+    def __init__(self, first_name, last_name, street_address, promise):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.street_address = street_address
+        self.promise = promise
+
+# Create a database of voter objects
+my_better_voters = {
+    "frodo@shire" : Voter("Frodo", "Baggins", "123 Underhill Lane", "destroy the ring"),
+    "sam@shire" : Voter("Sam", "Gamgee", "5 Gardeners Rd", "cater your wedding"),
+    "gandalf@istari" : Voter("Gandalf", "the Grey", "81 Wizard's Manor", "help you defeat Sauron"),
+    "sam@ecthelion" : Voter("Sam", "Fidor", "201 Tower Av", "rebuild the city")
+}
+
+# Write a function to produce a form letter using a voter object as an input
+def better_form_letter(voter):
+    print()
+    print(f"{voter.first_name} {voter.last_name}")
+    print(f"{voter.street_address}")
+    print(f"Gondor, Middle Earth")
+    print("")
+    print(f"Dear {voter.first_name}")
+    print(f"Thank you for voting for me. I promise to\n{voter.promise}")
+    print()
+    print("Sincerely,")
+    print("Lord Voldemort")
+
+# Produce the letters by evaluating the voters dictionary, pulling one 
+# voter object at a time and passing it to the form letter function.
+
+for voter in my_better_voters.values():
+    better_form_letter(voter)
