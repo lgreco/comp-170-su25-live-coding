241d306ae56f7c36a89829efd57b940c274f3ae5
Leo Irakliotis <lgreco@gmail.com>
2025-06-03 17:22:27 +0000

    03 jun live

---
+++ JUN04.py
@@ -0,0 +1,23 @@
+# JUN 04 live coding
+
+def f(x: int) -> int:
+    """Squares a given number.
+    
+    Input
+    -----
+    x : int
+      number to square 
+    
+    Returns
+    -------
+    x*x : int
+      the number squared"""
+    return ( x*x )
+
+
+
+# Basic function
+def g(x):
+    return x*x
+
+print(f(2.5))
