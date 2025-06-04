a2ed0887adfae01bfdc68f8f6426597871639cba
Leo Irakliotis <lgreco@gmail.com>
2025-06-04 16:26:37 +0000

    june4

---
+++ JUN03.py
@@ -0,0 +1,23 @@
+# JUN 03 live coding
+
+def f(x: int) -> int:
+    """Squares a given number.
+    
+    Input
+    -----
+    x : int
+      number to square 
+    
+    Returns
+    -------
+    int
+      the number squared
+    """
+    return x * x
+
+# Simple print
+print(f(5))
+
+# Fancy print
+x = 6
+print(f"\nThe square of {x} is {f(x)}\n")
