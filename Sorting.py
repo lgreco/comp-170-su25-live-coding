c77be8ffc1176748b28cc7d39c1f0d8b51f149cb
Leo Irakliotis <lgreco@gmail.com>
2025-07-21 17:22:11 +0000

    naive merge

--- Sorting.py
+++ Sorting.py
@@ -1,11 +1,11 @@
-def naive_sorting(our_data:list[int]):
-    # Consider a progressively decreasing version of 
+def naive_sorting(our_data: list[int]):
+    # Consider a progressively decreasing version of
     # our input array
-    for end in range(len(our_data)-1, 1, -1):
+    for end in range(len(our_data) - 1, 1, -1):
         # Find the largest element in this version of the array
         # Assume largest is the first element
         largest = 0
-        for i in range(1,end+1):
+        for i in range(1, end + 1):
             if our_data[i] > our_data[largest]:
                 largest = i
         # When the loop ends, largest points to the largest
@@ -13,5 +13,5 @@ def naive_sorting(our_data:list[int]):
         # at position end-1 and whatever was at position end-1
         # at position largest
         temp = our_data[largest]
-        our_data[largest] = our_data[end-1]
-        our_data[end-1] = temp
+        our_data[largest] = our_data[end - 1]
+        our_data[end - 1] = temp
